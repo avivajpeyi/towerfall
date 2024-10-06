4a2a5fcd505f437f595d4f4888d966998d7039a3
Ithrendrial <nsx9698@autuni.ac.nz>
2024-10-06 14:55:54 +1300

    added door and key combo

--- Entities/Door/door.gd
+++ Entities/Door/door.gd
@@ -6,14 +6,13 @@ var _num_required_keys:int = 0
 var _num_keys_collected: int = 0  # Keep track of the collected keys
 
 
-@onready var open_sprite: Sprite2D = $OpenSprite2D
-@onready var locked_sprite: Sprite2D = $LockedSprite2D
-@onready var collider: CollisionShape2D = $CollisionShape2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var static_body: StaticBody2D = $StaticBody2D
 var _is_locked: bool = true
 
 
 func _ready():
-	for key in get_tree().get_nodes_in_group("keys"):  # Ensure keys are in a group called "keys"
+	for key in get_tree().get_nodes_in_group("door_keys"):  # Ensure keys are in a group called "door_keys"
 		key.connect("key_collected", _on_key_collected)
 		_num_required_keys +=1 
 	_lock_door(true)
@@ -21,7 +20,7 @@ func _ready():
 
 func _on_key_collected():
 	_num_keys_collected += 1
-	print("Collected a key! Total keys:", _num_keys_collected)
+	print("Collected a key! Total keys: ", _num_keys_collected, " out of ", _num_required_keys)
 
 	if _num_keys_collected >= _num_required_keys:
 		_lock_door(false) 
@@ -31,11 +30,11 @@ func _lock_door(lock:bool):
 	_is_locked = lock
 	if lock:
 		print("Lock door!")
-		collider.disabled = false
-		open_sprite.visible = false
-		locked_sprite.visible = true
+		animated_sprite.play("locked")
+		static_body.set_collision_layer_value(1, true)  # Enable collision on layer 1
+		static_body.set_collision_mask_value(1, true)   # Enable collision mask on layer 1
 	else:
 		print("unlock door!")
-		collider.disabled = true
-		open_sprite.visible = true
-		locked_sprite.visible = false
+		animated_sprite.play("unlocked")
+		static_body.set_collision_layer_value(1, false)  # Disable collision on layer 1
+		static_body.set_collision_mask_value(1, false)   # Disable collision mask on layer 1
