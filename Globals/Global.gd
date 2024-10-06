b78407d69f9725c44966c5742c03d69e4c4cb25e
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-10-06 15:43:55 +1300

    add leaderbord linking

--- Globals/Global.gd
+++ Globals/Global.gd
@@ -3,17 +3,52 @@ extends Node2D
 
 var OST = load("res://Audio/OST.mp3")
 var audio
+var player_name:String
+
 
 func _ready():
+	_connect_to_leaderboard()
+	player_name = _generate_player_name()
 	await get_tree().create_timer(0.1).timeout
+	_add_gamemusic_loop()
+	
+
+
+func _add_gamemusic_loop():
 	audio = AudioStreamPlayer.new()
 	add_child(audio)
 	audio.stream = OST
 	audio.play()
 	audio.finished.connect(audio.play)
+	
 
 func _input(event):
 	if event.is_action_pressed("ui_fullscreen"):
 		var win_full = DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
 		DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_VISIBLE if win_full else DisplayServer.MOUSE_MODE_HIDDEN)
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED if win_full else DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+
+
+func _connect_to_leaderboard():
+	SilentWolf.configure({
+		"api_key": "xRcZ5BFPDK5AyEzuDjVmXaF8u4u1iA7UPNNX8KP6",
+		"game_id": "towerfall",
+		"game_version": "1.0.2",
+		"log_level": 1
+	})
+
+	SilentWolf.configure_scores({
+	"open_scene_on_close": "res://scenes/MainPage.tscn"
+	})
+	
+	
+
+func _generate_player_name() -> String:
+	var prefixes = ["Fli", "Twi", "Pip", "Bub", "Nim", "Doo", "Zee", "Mimi", "Lolo", "Tee"]
+	var suffixes = ["pie", "bin", "po", "dee", "boo", "zi", "bi", "lu", "lo", "ni"]
+
+	var prefix = prefixes[randi() % prefixes.size()]
+	var suffix = suffixes[randi() % suffixes.size()]
+	var number = randi() % 100  # Random number from 0 to 99
+	return "%s%s [%d]" % [prefix, suffix, number]
--- Globals/LevelManager.gd
+++ Globals/LevelManager.gd
@@ -1,6 +1,15 @@
 extends Node2D
 class_name LevelManager
 
+signal level_changed(level_num:int)
+
+
+
+
+
+
+
+
 var _level_idx = 0
 var _level_names = _load_level_paths()
 var _transition_settings:Dictionary = {
@@ -8,17 +17,26 @@ var _transition_settings:Dictionary = {
 		"pattern_leave": "squares",
 		"invert_on_leave": true,
 	}
+	
 
 
+var NLevels:int = len(_level_names)
+
+
+func entered_final_level()->bool:
+	return get_current_level_num() == NLevels
+
+func get_current_level_num():
+	return _level_idx + 1
 	
 func RestartLevel():
-	var current_scene = get_tree().current_scene
 	SceneManager.reload_scene(_transition_settings)
 
 func GoToNextLevel():
 	_level_idx+=1 
 	SceneManager.change_scene(_level_names[_level_idx], _transition_settings)
-
+	level_changed.emit(_level_idx)
+	
 func _load_level_paths()->PackedStringArray:
 	var level_files = FileUtils.find_files_of_type("res://Levels/levels/", "scn")
 	level_files = level_files.duplicate() as Array
@@ -26,6 +44,8 @@ func _load_level_paths()->PackedStringArray:
 	return PackedStringArray(level_files)
 
 func _compare_level_names(a: String, b: String) -> int:
-	var num_a = int(a.get_basename().split("_")[-1])  # Extracts the number after the last underscore
-	var num_b = int(b.get_basename().split("_")[-1])  # Extracts the number after the last underscore
-	return num_a - num_b
+	var basename_a = a.get_basename().split("_")
+	var basename_b = b.get_basename().split("_")
+	var num_a = int(basename_a[-1]) 
+	var num_b = int(basename_b[-1])
+	return num_a > num_b
